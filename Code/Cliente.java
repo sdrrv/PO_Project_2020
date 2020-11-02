6ff0ae4fd760b44c47b57e98be3b8f398ee8eba9
Duarte Elvas <duartecelvas@gmail.com>
2020-11-02 11:07:00 +0000

    Added the Pessoa adn client Class

---
+++ Code/Cliente.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Cliente extends Pessoa{
+    public int _pontos;
+
+    public Cliente(String nome, String morada, int key){
+        super(nome, morada, key);
+        _pontos=0;
+    }
+
+    public String getEstatuto(){}// to add 
+    
+}
