4fed97690e9454b5c3ae6f56dc7ba9122fa08c87
Duarte Elvas <duartecelvas@gmail.com>
2020-11-09 00:22:38 +0000

    DoRegisterSupplier

--- src/woo/app/suppliers/DoRegisterSupplier.java
+++ src/woo/app/suppliers/DoRegisterSupplier.java
@@ -3,23 +3,34 @@ package woo.app.suppliers;
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.DialogException;
 import pt.tecnico.po.ui.Input;
+import woo.app.exception.DuplicateProductKeyException;
 import woo.core.StoreManager;
-//FIXME import other classes
+import woo.core.exception.DuplicateSupplierIdException;
 
 /**
  * Register supplier.
  */
 public class DoRegisterSupplier extends Command<StoreManager> {
 
-  //FIXME add input fields
+  private Input<String> _suppplierKey;
+  private Input<String> _name;
+  private Input<String> _address;
 
   public DoRegisterSupplier(StoreManager receiver) {
     super(Label.REGISTER_SUPPLIER, receiver);
-    //FIXME init input fields
+    _suppplierKey= _form.addStringInput(Message.requestSupplierKey());
+    _name= _form.addStringInput(Message.requestSupplierName());
+    _address= _form.addStringInput(Message.requestSupplierAddress());
   }
 
   @Override
   public void execute() throws DialogException {
-    //FIXME implement command
+    _form.parse();
+    try{
+      _receiver.registerSupplier(_name.value(),_address.value(),_suppplierKey.value());
+    }
+    catch (DuplicateSupplierIdException e){
+      throw new DuplicateProductKeyException(e.getKey());
+    }
   }
 }
