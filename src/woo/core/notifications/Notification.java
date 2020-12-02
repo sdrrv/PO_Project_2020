f891b6e2caa3e530492095fa92f23bc1a290ab10
Duarte Elvas <duartecelvas@gmail.com>
2020-12-02 22:06:30 +0000

    Skeleton of the notification class done

--- src/woo/core/notifications/Notification.java
+++ src/woo/core/notifications/Notification.java
@@ -1,5 +1,25 @@
 package woo.core.notifications;
 
 public class Notification {
-    C
+    private int _id;
+    private Description _desc;
+    private DeliveryMethod _deliMethod;
+
+    public Notification(int id, Description desc, DeliveryMethod deliMethod){
+        _id=id;
+        _desc = desc;
+        _deliMethod = deliMethod;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public Description getDesc() {
+        return _desc;
+    }
+
+    public DeliveryMethod getDeliMethod() {
+        return _deliMethod;
+    }
 }
